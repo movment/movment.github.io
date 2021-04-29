ad1c5e7e99658036296ca9dfc1c35568ca0a241f
Dongin Kim <miknignod@gmail.com>
2021-04-29 15:00:15 +0900

    IE11 지원

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
